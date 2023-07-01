0b60cbf349b84dea8909cd6229116ab09f92f21e
Mrinmay <mrinmaydas2604@gmail.com>
2023-07-02 02:52:51 +0530

    okiee

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Hero() {
+    return(
+        <>
+        <div class="image-blob">
+        <img src="https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr" alt="" class="blob-image"/>
+        
+        </div>
+
+        </>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar(props) {
+    return (
+        <div className="navbar">
+    <img className="navbar-logo" src="/newbie.png" alt="Logo"/>
+    <span className="navbar-text">Mrinmay Das</span>
+    <button className="contact-button">Contact Me</button>
+  </div>
+    )
+}
